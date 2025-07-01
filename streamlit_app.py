70166c49bfa243dacd9a78e4065ecf69594defeb
petervdv-snowflake <petervdv@mweb.co.za>
2025-07-01 15:01:42 +0200

    Update streamlit_app.py
    Change to pandas dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,9 @@ session = cnx.session()
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
-st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+# st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 ingredients_list = st.multiselect(
